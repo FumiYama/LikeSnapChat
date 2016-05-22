0019bd249ec28c66a6027a8066e6b2c57af32f05
FumiYama <f.yamanaka1127@gmail.com>
2016-05-22 16:18:29 +0900

    first commit

--- LikeSnapChat/ViewController.swift
+++ LikeSnapChat/ViewController.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+//        View1
+        let V1: View1 = View1(nibName: "View1", bundle: nil)
+        
+        self.addChildViewController(V1)
+        self.scrollView.addSubview(V1.view)
+        V1.didMoveToParentViewController(self)
+        
+//        View2
+        let V2: View2 = View2(nibName: "View2", bundle: nil)
+        
+        self.addChildViewController(V2)
+        self.scrollView.addSubview(V2.view)
+        V2.didMoveToParentViewController(self)
+        
+        var V2Frame: CGRect = V2.view.frame
+        V2Frame.origin.x = self.view.frame.width
+        V2.view.frame = V2Frame
+        
+//        View3
+        let V3: View3 = View3(nibName: "View3", bundle: nil)
+
+        self.addChildViewController(V3)
+        self.scrollView.addSubview(V3.view)
+        V3.didMoveToParentViewController(self)
+        
+        var V3Frame: CGRect = V3.view.frame
+        V3Frame.origin.x = self.view.frame.width * 2
+        V3.view.frame = V3Frame
+
+        
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width * 3, self.view.frame.size.height)
+        
     }
 
     override func didReceiveMemoryWarning() {
